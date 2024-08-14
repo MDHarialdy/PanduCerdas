6268d3810929f0a5b0666a90590ff6c7a00c8a82
M DOLYANDA HARIALDY <yayangharialdyy@gmail.com>
2024-08-14 15:33:56 +0700

    update

--- app/src/main/java/com/panducerdas/id/TesEngine.kt
+++ app/src/main/java/com/panducerdas/id/TesEngine.kt
@@ -1,20 +1,23 @@
 package com.panducerdas.id
 
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+
 
 class TesEngine : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_tes_engine)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+    }
+
+    private fun chekPermissionStorage(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED){
+
         }
     }
 }
