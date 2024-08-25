0bf3157895e73ef7eb7ceeb288e594ab496c2372
M Dolyanda Harialdy <yayangharialdyy@gmail.com>
2024-08-25 21:53:19 +0700

    update

--- app/src/main/java/com/panducerdas/id/ui/MainActivity.kt
+++ app/src/main/java/com/panducerdas/id/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.panducerdas.id
+package com.panducerdas.id.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.panducerdas.id.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
